980ce0642a0682549ca713d7262b8be6f25b39bc
asforest <asforest@qq.com>
2022-08-10 12:06:12 +0800

    refactor: remove redundant $

--- src/main/kotlin/com/github/balloonupdate/exception/HttpResponseStatusCodeException.kt
+++ src/main/kotlin/com/github/balloonupdate/exception/HttpResponseStatusCodeException.kt
@@ -3,4 +3,4 @@ package com.github.balloonupdate.exception
 import com.github.balloonupdate.util.Utils
 
 class HttpResponseStatusCodeException(statusCode: Int, url: String, body: String?)
-    : BaseException("Http状态码($statusCode)不在2xx-3xx之间(${Utils.getUrlFilename(url)})\n$url\n$${body ?: "<No Body>"}")
+    : BaseException("Http状态码($statusCode)不在2xx-3xx之间(${Utils.getUrlFilename(url)})\n$url\n${body ?: "<No Body>"}")
