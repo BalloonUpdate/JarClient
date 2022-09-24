cca034657c271c61c73d5e19e3303db432f7f536
KasumiNova <3044344887@qq.com>
2022-09-25 00:10:33 +0800

    改进

--- src/main/java/com/github/kasuminova/Utils/FileUtil.java
+++ src/main/java/com/github/kasuminova/Utils/FileUtil.java
@@ -2,23 +2,6 @@ package com.github.kasuminova.Utils;
 
 public class FileUtil {
     /**
-     * 根据传入大小返回合适的大小格式化文本
-     * @param size 文件大小
-     * @return Byte 或 KB 或 MB 或 GB
-     */
-    public static String formatFileSizeToStr(long size) {
-        if (size <= 1024) {
-            return size + " Byte";
-        } else if (size <= 1024 * 1024) {
-            return String.format("%.2f", (double) size / 1024) + " KB";
-        } else if (size <= 1024 * 1024 * 1024) {
-            return String.format("%.2f", (double) size / (1024 * 1024)) + " MB";
-        } else {
-            return String.format("%.2f", (double) size / (1024 * 1024 * 1024)) + " GB";
-        }
-    }
-
-    /**
      * 根据传入大小返回合适的 int 大小
      * 适用于网络文件上传
      * @param size 文件大小
